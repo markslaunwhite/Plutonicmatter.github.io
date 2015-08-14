9d7d30a64ed75bf2792cfee2420a16f40d517211
Cole Perkins <colesperks@gmail.com>
2015-08-13 21:03:11 -0300

    js changed for more responsive rendering

--- js/navbar.js
+++ js/navbar.js
@@ -1,45 +1,67 @@
+// $(function() {
+//     // $(".bounce").delay(4000).fadeIn(500);
+
+
+// window.onscroll = function() {
+
+//     }
+
+// $(".bounce").hide(0).delay(5000).fadeIn(1000).show(0);
+// $("#nav-bar").hide(0);
+// $("#mail-icon").hide(0);
+
+// window.onscroll = function() {
+// if (window.pageYOffset >= 200){
+//             $('.bounce').fadeOut(500).hide(0);
+//         } else if (window.pageYOffset <= 10){
+//             $('.bounce').fadeIn(500).show(0);
+//         }
+
+
+//         if (window.pageYOffset >= 300){
+//             $('#nav-bar').fadeIn(500).show(0);
+//         } else if (window.pageYOffset <= 200){
+//             $('#nav-bar').fadeOut(500).hide(0);
+//         }
+
+
+//     if (window.pageYOffset >= 400){
+//             $('#mail-icon').fadeIn(500).show(0);
+//         // } else if (window.pageYOffset <= 10){
+//         //     $('#mail-icon').fadeOut(500).hide(0);
+//         }
+
+
+
+//     }
+
+// });
 $(function() {
-    // $(".bounce").delay(4000).fadeIn(500);
+  $("#mail-icon").hide(0);
+    $(window).scroll(function() {
+
+var mailTargetPercentage = 10;
+    scrollTo = $(window).scrollTop(),
+    docHeight = $(document).height(),
+    windowHeight = $(window).height();
+    scrollPercent = (scrollTo / (docHeight-windowHeight)) * 100;
+// to round to nearest whole number
+    scrollPercent = scrollPercent.toFixed(1);
+// console.log(docHeight);
+
+// console.log(windowHeight);
+console.log(scrollPercent);
+// console.log(mailTargetPercentage);
 
 
-window.onscroll = function() {
+if(scrollPercent > mailTargetPercentage) {
+  $('#mail-icon').fadeIn(500).show(0)
+  }else if (scrollPercent < mailTargetPercentage) {
+  $('#mail-icon').fadeOut(500).hide(0);
+  }
 
-    }
-
-$(".bounce").hide(0).delay(5000).fadeIn(1000).show(0);
-$("#nav-bar").hide(0);
-$("#mail-icon").hide(0);
-
-window.onscroll = function() {
-if (window.pageYOffset >= 200){
-            $('.bounce').fadeOut(500).hide(0);
-        } else if (window.pageYOffset <= 10){
-            $('.bounce').fadeIn(500).show(0);
-        }
-
-
-        if (window.pageYOffset >= 300){
-            $('#nav-bar').fadeIn(500).show(0);
-        } else if (window.pageYOffset <= 200){
-            $('#nav-bar').fadeOut(500).hide(0);
-        }
-
-
-    if (window.pageYOffset >= 400){
-            $('#mail-icon').fadeIn(500).show(0);
-        // } else if (window.pageYOffset <= 10){
-        //     $('#mail-icon').fadeOut(500).hide(0);
-        }
-
-
-
-    }
-
+}).trigger('scroll');
 });
-
-
-
-
 
 
 jQuery(document).ready(function($){
