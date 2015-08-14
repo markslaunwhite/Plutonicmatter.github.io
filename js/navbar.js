3250e6f7e9a773929d65e06d60b619dd04874d68
Cole Perkins <colesperks@gmail.com>
2015-08-14 16:34:20 -0300

    js updated function to not load on contact page

--- js/navbar.js
+++ js/navbar.js
@@ -1,87 +1,59 @@
 
 jQuery(document).ready(function($){
-// $(function() {
-//     // $(".bounce").delay(4000).fadeIn(500);
-
-
-// window.onscroll = function() {
-
-//     }
-
-// $(".bounce").hide(0).delay(5000).fadeIn(1000).show(0);
-// $("#nav-bar").hide(0);
-// $("#mail-icon").hide(0);
-
-
-
-
-
-// window.onscroll = function() {
-// if (window.pageYOffset >= 200){
-//             $('.bounce').fadeOut(500).hide(0);
-//         } else if (window.pageYOffset <= 10){
-//             $('.bounce').fadeIn(500).show(0);
-//         }
-
-
-//         if (window.pageYOffset >= 300){
-//             $('#nav-bar').fadeIn(500).show(0);
-//         } else if (window.pageYOffset <= 200){
-//             $('#nav-bar').fadeOut(500).hide(0);
-//         }
-
-
-//     if (window.pageYOffset >= 400){
-//             $('#mail-icon').fadeIn(500).show(0);
-//         // } else if (window.pageYOffset <= 10){
-//         //     $('#mail-icon').fadeOut(500).hide(0);
-//         }
-
-
-
-//     }
-
-// });
-
   $("#mail-icon").hide(0);
   $(".bounce").hide(0).delay(5000).fadeIn(1000).show(0);
   $("#nav-bar").hide(0);
-$(window).scroll(function () {
 
-    var mailTargetPercentage = 15;
-    var arrowTargetPercentage = 5;
-    var navBarTargetPercentage = 5;
+  var onContactPage = function(){
 
-    scrollTo = $(window).scrollTop(),
-    docHeight = $(document).height(),
-    windowHeight = $(window).height();
-    scrollPercent = (scrollTo / (docHeight-windowHeight)) * 100;
+  // these are to locate the current page name you are on
+  var currentPage = window.location.pathname;
+  var contactPage = "/contact.html";
+
+// place the nav bar at the top of page to ensure it does not conflict with form
+// don't show the mail button
+  if (currentPage === contactPage) {
+    $('#nav-bar').css("position", "absolute").show(0);
+    $('#mail-icon').hide(0);
+  } else {
+    $(window).scroll(function () {
+
+      // set the percentage to activate fade in
+      var mailTargetPercentage = 15;
+      var arrowTargetPercentage = 5;
+      var navBarTargetPercentage = 5;
+
+      // checks doc height
+      scrollTo = $(window).scrollTop(),
+      docHeight = $(document).height(),
+      windowHeight = $(window).height();
+      scrollPercent = (scrollTo / (docHeight-windowHeight)) * 100;
+
+
 // to round to nearest whole number
     scrollPercent = scrollPercent.toFixed(1);
 
 
-if(scrollPercent > mailTargetPercentage) {
-  $('#mail-icon').fadeIn(500).show(0)
-  }else if (scrollPercent = 1) {
-  $('#mail-icon').fadeOut(500).hide(0);
-  }
+      if(scrollPercent > mailTargetPercentage) {
+        $('#mail-icon').fadeIn(500).show(0)
+      }
 
-if(scrollPercent > arrowTargetPercentage) {
-  $('.bounce').fadeIn(500).show(0)
-  }
+      if(scrollPercent > arrowTargetPercentage) {
+        $('.bounce').fadeIn(500).show(0)
+      }
 
-if(scrollPercent > navBarTargetPercentage) {
-  $('#nav-bar').fadeIn(500).show(0)
-  }else if (scrollPercent < mailTargetPercentage) {
-  $('#nav-bar').fadeOut(500).hide(0);
-  }
+      if(scrollPercent > navBarTargetPercentage) {
+        $('#nav-bar').fadeIn(500).show(0)
+      }
+    }).trigger('scroll');
+}
+}
 
-}).trigger('scroll');
+  onContactPage();
 
 
-$('input, text-area').on('click', function(){
-  $("#mail-icon").fadeOut(500).hide(0);
-});
+
+
 
 });
 
